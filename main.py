4b85575e430f1a262e2114b4567a7dfe394b6f55
Caleb Raughley <calebraughley@gmail.com>
2026-03-25 11:00:14 -0400

    Response

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def func2(data):
     print('Here is the data:')
     print(data)
 
-    choice = ''
+    response = ''
     while True:
-        choice = input('Select an option. 1-sum  2-min  3-max  4-quit')
-        if choice == '1':
+        response = input('Select an option. 1-sum  2-min  3-max  4-quit')
+        if response == '1':
             print('The sum is',sum(data))
-        elif choice == '2':
+        elif response == '2':
             print('Not supported yet')
-        elif choice == '3':
+        elif response == '3':
             print('Not supported yet')
-        elif choice == '4':
+        elif response == '4':
             print('Goodbye')
             return
 
